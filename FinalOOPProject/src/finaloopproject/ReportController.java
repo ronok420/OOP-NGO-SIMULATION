a047d101cd4de3a8d72d6e2641a692e779185baa
ronok420 <82023374+ronok420@users.noreply.github.com>
2023-08-27 11:03:09 +0600

    Add files via upload

---
+++ FinalOOPProject/src/finaloopproject/ReportController.java
@@ -0,0 +1,51 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/javafx/FXMLController.java to edit this template
+ */
+package finaloopproject;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+/**
+ * FXML Controller class
+ *
+ * @author usnot
+ */
+public class ReportController implements Initializable {
+
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+
+    @FXML
+    private void generateReportOnClick(ActionEvent event) {
+    }
+
+    @FXML
+    private void publishReportOnCLick(ActionEvent event) {
+    }
+
+    @FXML
+    private void backbutton(ActionEvent event) throws IOException {
+         Parent scene2Parent = FXMLLoader.load(getClass().getResource("Researcher.fxml"));//here switching scene fxml should be attached
+Scene scene2 = new Scene(scene2Parent);
+Stage stg2 = (Stage)((Node)event.getSource()).getScene().getWindow();  
+stg2.setScene(scene2);
+stg2.show();
+    }
+    
+}
